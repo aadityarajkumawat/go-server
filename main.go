741a694643a050945b687140de1307261882f829
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-07-28 20:22:47 +0530

    storing userid as cookies

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
@@ -15,39 +17,82 @@ const (
 	dbname   = "test_db"
 )
 
-// type user struct {
-// 	UserID   string `json:"userID"`
-// 	Username string `json:"username"`
-//}
+type user struct {
+	UserID   string `json:"userID"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
-// func getAlbums(c *gin.Context) {
-// 	c.IndentedJSON(http.StatusOK, albums)
-// }
-
-// func postAlbums(c *gin.Context) {
-// 	var newAlbum album
-
-// 	if err := c.BindJSON(&newAlbum); err != nil {
-// 		return
-// 	}
-
-// 	albums = append(albums, newAlbum)
-// 	c.IndentedJSON(http.StatusCreated, newAlbum)
-// }
+type registeredUserResponse struct {
+	Status string `json:"status"`
+}
 
 type dbStore struct {
 	db *sql.DB
 }
 
-func (conn *dbStore) connectToDB(connStr string) *sql.DB {
+type CookieStore struct {
+}
+
+var sessions = []CookieStore{}
+
+func saveUserToDB(conn *dbStore, newUser user) error {
+	var query = `INSERT INTO users(username, password) VALUES ($1, $2) RETURNING user_id;`
+	_, err := conn.db.Exec(query, newUser.Username, newUser.Password)
+	return err
+}
+
+func (conn *dbStore) registerUser(c *gin.Context) {
+	var newUser user
+	var response registeredUserResponse
+
+	if err := c.BindJSON(&newUser); err != nil {
+		return
+	}
+
+	var query = `INSERT INTO users(username, password) VALUES ($1, $2) RETURNING user_id;`
+	_, err := conn.db.Exec(query, newUser.Username, newUser.Password)
+
+	//err := saveUserToDB(conn, newUser)
+	if err != nil {
+		response.Status = "Server Error"
+		c.IndentedJSON(http.StatusBadRequest, response)
+		return
+	}
+	response.Status = "Account created!"
+
+	var rows *sql.Rows
+	rows, err = conn.db.Query("SELECT * FROM users WHERE username = $1 LIMIT 1", newUser.Username)
+	checkError(err)
+
+	var loggedUser = ""
+
+	if rows.Next() {
+		var userID string
+		var username string
+		var password string
+		err = rows.Scan(&userID, &username, &password)
+		checkError(err)
+
+		loggedUser = userID
+	}
+
+	expiration := time.Now().Add(365 * 24 * time.Hour)
+	cookie := http.Cookie{Name: "sid", Value: loggedUser, Expires: expiration, HttpOnly: true}
+	http.SetCookie(c.Writer, &cookie)
+	c.IndentedJSON(http.StatusCreated, response)
+}
+
+func (conn *dbStore) connectToDB(connStr string) {
 	var err error
-	var db = (*conn).db
+	var db *sql.DB
 
 	db, err = sql.Open("postgres", connStr)
 	checkError(err)
 
+	conn.db = db
+
 	fmt.Printf("\nSuccessfully connected to database!\n")
-	return db
 }
 
 func (conn dbStore) closeConnection() {
@@ -64,6 +109,7 @@ func checkError(err error) {
 func (conn dbStore) testConnection() {
 	err := conn.db.Ping()
 	checkError(err)
+	fmt.Println("Connection Tested, Success!")
 }
 
 func main() {
@@ -74,23 +120,34 @@ func main() {
 		password,
 		host)
 
-	// ***********Store object**************
-	dbStoreI := dbStore{ db: nil }
+	// *********** Store instance **************
+	dbStoreI := dbStore{db: nil}
 
+	// *********** Connecting to DB **************
 	dbStoreI.connectToDB(connStr)
 	defer dbStoreI.closeConnection()
 
+	// *********** Testing connection **************
 	dbStoreI.testConnection()
 
-	//sqlStatement := `INSERT INTO students (name) VALUES ($1)`
-	//_, err := db.Exec(sqlStatement, "Aditya")
-	//if err != nil {
-	//	panic(err)
-	//} else {
-	//	fmt.Println("\nRow inserted successfully!")
+	//var rows *sql.Rows
+	//rows, err := dbStoreI.db.Query("SELECT * FROM users")
+	//checkError(err)
+	//
+	//for rows.Next() {
+	//	var userID string
+	//	var username string
+	//	var password string
+	//
+	//	err = rows.Scan(&userID, &username, &password)
+	//	checkError(err)
+	//
+	//	fmt.Println("userID | username | password")
+	//	fmt.Printf("%s | %s | %s\n", userID, username, password)
 	//}
-	// router.GET("/albums", getAlbums)
-	// router.POST("/albums", postAlbums)
+
+	router.POST("/register", dbStoreI.registerUser)
+
 	err := router.Run("localhost:8080")
 	checkError(err)
 }
